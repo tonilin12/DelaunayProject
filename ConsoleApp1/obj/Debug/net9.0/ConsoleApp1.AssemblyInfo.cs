2c97dd6e5822f8ecb568ecf2c820d2ef9ae2c39a
tonilin12 <linweitao12@gmail.com>
2026-07-30 23:40:05 +0200

    revert back

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b9edffa3d06297cc15d63a88f4a055cee94de0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db8c9f1de30a5d2c57ae71add992005111f0646")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
